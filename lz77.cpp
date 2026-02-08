fe573b435c6dced684e28bc225864693d6c14748
jvtoppa <jvtoppa@gmail.com>
2026-02-08 17:32:00 -0300

    feat: circular buffer

--- lz77.cpp
+++ lz77.cpp
@@ -27,15 +27,53 @@ class CircularBuffer
     {
         buffer[head] = c;
         head = (head + 1) % capacity;
-        if(bytes_stored < capacity)
-        {
-            bytes_stored++;
-        }
+
+        if(bytes_stored < capacity) bytes_stored++;
+        
         return;
     }
 
+    char get_newest(size_t offset)
+    {
+        if(offset >= bytes_stored)
+        {
+            throw out_of_range("Offset too large.\n");
+        }
 
+        return buffer[(head - offset - 1 + capacity) % capacity];
     
+    }
+
+    char get_oldest(size_t index)
+    {
+        if(index >= bytes_stored)
+        {
+            throw out_of_range("Index out of bounds.\n");
+        }
+
+        return buffer[(((head - bytes_stored + capacity) % capacity) + index) % capacity];
+    }
+
+    size_t size()
+    {
+        return bytes_stored;
+    }
+
+    void clear()
+    {
+        head = 0;
+        bytes_stored = 0;
+        return;
+    }
+    
+    void print()
+    {
+        cout << "Buffer [" << bytes_stored << "/" << capacity << "]: ";
+        for (size_t i = 0; i < bytes_stored; i++) {
+            cout << get_oldest(i) << ", ";
+        }
+        cout << endl;
+    }
 };
 
 
